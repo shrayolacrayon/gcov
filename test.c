2b369aa5c5da4b5152e9172baf549057a5f893a6
sr527 <sr527@cornell.edu>
2012-06-07 13:13:46 -0400

    first commit

---
+++ test.c
@@ -0,0 +1,24 @@
+//test file for gcov
+int gcd (int a, int b);
+int main (){
+	int sum = 0;
+	int evens = 0;
+	int i;
+	for ( i = 0; i < 10000; i++)
+	{
+	sum += i;
+	if (sum % 2 == 0)
+	{
+		evens += i;
+	}
+	}
+	gcd(3502, 42);
+	
+}
+
+int gcd(int a, int b){
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, (a % b));
+}
