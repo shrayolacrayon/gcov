1236f3f99711c6b8cde82fa12e8306b515cfe4e6
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-07 15:38:43 -0400

    fix bugs in printStart

--- printStart.c
+++ printStart.c
@@ -1,7 +1,9 @@
 #include "printStart.h"
+#include <stdio.h>
+
 void printStart(char* filename){
-	char str;
-	FILE *f = fopen(argv[1], "r"); //opens up a file pointer for reading
+	char str[1024];
+	FILE *f = fopen(filename, "r"); //opens up a file pointer for reading
 	int nb = fread(str, 1,1023,f);
 	str[nb] = 0;
 	printf("%s \n ", str);
