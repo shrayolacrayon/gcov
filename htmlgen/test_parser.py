cddde4dad0b452e6617683d7256d32efb8c0603f
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-08 18:04:38 -0400

    add test script

---
+++ htmlgen/test_parser.py
@@ -0,0 +1,14 @@
+from parser import TracefileParser
+import sys
+
+if __name__ == '__main__':
+    tp = TracefileParser(sys.argv[1])
+    
+    
+    print tp.full_statistics()
+    
+    for directory in tp.list_dirs():
+        print tp.full_statistics(directory)
+        for filename in tp.list_files(directory):
+            print tp.full_statistics(directory, filename)
+
