3c35b1e361d37fa3eb11a6d8e3078a1f2032aff5
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-08 15:03:40 -0400

    add base classes

---
+++ htmlgen/gcov.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from collections import namedtuple
+
+Branch = namedtuple('Branch', ['lineno', 'path', 'times'])
+Function = namedtuple('Function', ['name', 'lineno', 'times'])
+Line = namedtuple('Line', ['lineno', 'times'])
+
+def pair():
+    return [None, None]
+
+class Source:
+    def __init__(self):
+        self.branches = defaultdict(pair)
+        self.functions = {}
+        self.lines = {}
+
+    def add_branch(self, branch):
+        self.branches[branch.lineno][branch.path] = branch
+
+    def add_function(self, func):
+        self.functions[func.lineno] = func
+
+    def add_line(self, line):
+        self.lines[line.lineno] = line
