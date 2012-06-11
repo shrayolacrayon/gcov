0fe6a6e03dec2793ce05def1063bd3cfafb14a30
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-11 09:54:49 -0400

    fix bugs in generators.py

--- gcov/generators.py
+++ gcov/generators.py
@@ -1,6 +1,7 @@
+from __future__ import division
 from jinja2 import Environment, PackageLoader
 
-env = Environment(loader = PackageLoader('gcov', 'templates'))
+env = Environment(loader = PackageLoader('gcov'))
 
 def front_page(tracefile):
     template = env.get_template('index.html')
@@ -8,11 +9,12 @@ def front_page(tracefile):
     bhits, btotal = tracefile.branch_coverage()
     lhits, ltotal = tracefile.line_coverage()
 
-    fperc = fhits ./ ftotal * 100
-    bperc = bhits ./ btotal * 100
-    lperc = lhits ./ ltotal * 100
+    fperc = fhits / ftotal * 100
+    bperc = bhits / btotal * 100
+    lperc = lhits / ltotal * 100
 
     return template.render(directories = tracefile.list_dirs(), 
-                           fhits = fhits, ftotal = ftotal, fperc = fperc
-                           bhits = bhits, btotal = btotal, bperc = bperc
+                           basepath = tracefile.basepath,
+                           fhits = fhits, ftotal = ftotal, fperc = fperc,
+                           bhits = bhits, btotal = btotal, bperc = bperc,
                            lhits = lhits, ltotal = ltotal, lperc = lperc)
