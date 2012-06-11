03fc5516cbe3ab178acdb726b93143ebbac2ddb6
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-11 09:33:48 -0400

    starting work on generators.py

---
+++ gcov/generators.py
@@ -0,0 +1,18 @@
+from jinja2 import Environment, PackageLoader
+
+env = Environment(loader = PackageLoader('gcov', 'templates'))
+
+def front_page(tracefile):
+    template = env.get_template('index.html')
+    fhits, ftotal = tracefile.function_coverage()
+    bhits, btotal = tracefile.branch_coverage()
+    lhits, ltotal = tracefile.line_coverage()
+
+    fperc = fhits ./ ftotal * 100
+    bperc = bhits ./ btotal * 100
+    lperc = lhits ./ ltotal * 100
+
+    return template.render(directories = tracefile.list_dirs(), 
+                           fhits = fhits, ftotal = ftotal, fperc = fperc
+                           bhits = bhits, btotal = btotal, bperc = bperc
+                           lhits = lhits, ltotal = ltotal, lperc = lperc)
